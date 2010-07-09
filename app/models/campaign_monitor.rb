a2a2d92c7bcdd2da4a54414c876af4ddee5ac9d4
Roman Simecek <roman.simecek@screenconcept.ch>
2010-07-09 14:36:31 +0200

    campaign monitor settings via backend modifiable

--- app/models/campaign_monitor.rb
+++ app/models/campaign_monitor.rb
@@ -5,6 +5,12 @@ class CampaignMonitor < ActiveRecord::Base
   validates_uniqueness_of :active, :if => :active
   validates_presence_of :api_key, :name
 
+  named_scope :active, :conditions => {:active => true}
+
+  def self.form_fields
+    [:name, :api_key, :client_id, :list_id, :active]
+  end
+
   private
 
   #
@@ -12,8 +18,10 @@ class CampaignMonitor < ActiveRecord::Base
   #
   def active_to_inactive
     campaign = CampaignMonitor.find_by_active(true)
-    campaign.active = false
-    campaign.save
+    if self != campaign and campaign != nil
+      campaign.active = false
+      campaign.save
+    end
   end
 
 end
