4f61fcf62f1d5c87d8d95bfd4fe3bf4362901b2d
Roman Simecek <roman.simecek@screenconcept.ch>
2010-07-09 16:27:33 +0200

    now only one compaign monitor in db stored

--- app/models/campaign_monitor.rb
+++ app/models/campaign_monitor.rb
@@ -1,27 +1,9 @@
 class CampaignMonitor < ActiveRecord::Base
 
-  before_validation :active_to_inactive, :if => :active
-
-  validates_uniqueness_of :active, :if => :active
   validates_presence_of :api_key, :name
 
-  named_scope :active, :conditions => {:active => true}
-
   def self.form_fields
-    [:name, :api_key, :client_id, :list_id, :active]
-  end
-
-  private
-
-  #
-  # Sets the active record to inactive
-  #
-  def active_to_inactive
-    campaign = CampaignMonitor.find_by_active(true)
-    if self != campaign and campaign != nil
-      campaign.active = false
-      campaign.save
-    end
+    [:name, :api_key, :client_id]
   end
 
 end
