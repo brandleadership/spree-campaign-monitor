df915f8c1c2901fbe741ccfc468d34724c85e096
Roman Simecek <roman.simecek@screenconcept.ch>
2010-07-20 08:29:59 +0200

    some unkown changes for campaign monitor

--- app/models/newsletter_subscription_observer.rb
+++ app/models/newsletter_subscription_observer.rb
@@ -6,17 +6,36 @@ class NewsletterSubscriptionObserver < ActiveRecord::Observer
 
 
   def after_create(newsletter_subscription)
-    subscriber = Campaigning::Subscriber.new(newsletter_subscription.email,
+    answer = campaign_subscriber(newsletter_subscription).add!(CampaignMonitor.active.first.list_id)
+    unless answer.code.eql?0
+      newsletter_subscription.active = false
+      newsletter_subscription.save
+    end
+  end
+
+  def after_update(newsletter_subscription)
+    subscription = campaign_subscriber(newsletter_subscription)
+    list = CampaignMonitor.active.first.list_id
+    unless newsletter_subscription.active
+      if subscription.is_subscribed?(list)
+        subscription.unsubscribe!(list)
+      end
+    else
+      subscription.add_and_resubscribe!(list)
+    end
+  end
+
+  private
+
+  #
+  # Returns a Campaigning::Subscriber class generated from newsletter subscription model
+  #
+  def campaign_subscriber(newsletter_subscription)
+    Campaigning::Subscriber.new(newsletter_subscription.email,
                                              newsletter_subscription.first_name + " " + newsletter_subscription.last_name,
                                              Time.now,
                                              true,
                                              nil,
                                              {:apiKey => CampaignMonitor.active.first.api_key})
-    subscriber.add!(CampaignMonitor.active.first.list_id)
   end
-
-  def after_update(newsletter_subscription)
-    puts newsletter_subscription
-  end
-  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
  map.namespace :admin do |admin|
+   admin.resources :campaign_lists
+   admin.resources :campaign_campaigns
    admin.resources :campaign_monitors
    admin.resources :newsletter_subscriptions
    admin.download_as_csv '/admin/newsletter_subscriptions/download' , :controller =>'newsletter_subscriptions', :action => 'download_as_csv'
