57495bfc668579e3a9bc0f3969f11ed386524b08
raskhadafi <roman.simecek@screenconcept.ch>
2010-07-29 12:36:24 +0200

    campaign list shown in the campaign configuration view

--- app/helpers/admin/campaign_monitors_helper.rb
+++ app/helpers/admin/campaign_monitors_helper.rb
@@ -1,2 +1,10 @@
 module Admin::CampaignMonitorsHelper
+
+  #
+  # True when campaign monitor is defined
+  #
+  def defined_campaign_monitor
+    CampaignMonitor.find(:first) ? true : false
+  end
+
 end
