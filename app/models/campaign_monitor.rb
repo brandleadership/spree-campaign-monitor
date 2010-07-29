8a3ad1dfa505f96d1a137ae5568df6f4aedba8d0
raskhadafi <roman.simecek@screenconcept.ch>
2010-07-29 16:44:30 +0200

    updates now also the campaigns

--- app/models/campaign_monitor.rb
+++ app/models/campaign_monitor.rb
@@ -1,6 +1,7 @@
 class CampaignMonitor < ActiveRecord::Base
 
   has_many :campaign_lists, :dependent => :delete_all
+  has_many :campaigns, :dependent => :delete_all
 
   validates_presence_of :api_key, :name, :client_key
 
@@ -18,6 +19,13 @@ class CampaignMonitor < ActiveRecord::Base
     client = Campaigning::Client.new(self.client_key, self.name, {:apiKey => self.api_key})
     client.lists.each do |list|
       self.campaign_lists.create(:name => list.name, :list_key => list.listID) unless CampaignList.find(:first, :conditions => {:list_key => list.listID})
+    end
+    client.campaigns.each do |campaign|
+      self.campaigns.create(:campaign_key => campaign.campaignID,
+                           :subject => campaign.subject,
+                           :name => campaign.name,
+                           :sent_date => campaign.sentDate,
+                           :total_recipients => campaign.totalRecipients) unless Campaign.find(:first, :conditions => { :campaign_key => campaign.campaignID })
     end
   end
 
