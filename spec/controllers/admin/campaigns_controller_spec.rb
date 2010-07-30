5867c4e61dbb5ad7b61316648303b6a4c082d125
raskhadafi <roman.simecek@screenconcept.ch>
2010-07-30 08:27:21 +0200

    renaming cause of refactoring

--- spec/controllers/admin/campaigns_controller_spec.rb
+++ spec/controllers/admin/campaigns_controller_spec.rb
@@ -1,10 +1,10 @@
 require File.dirname(__FILE__) + '/../../spec_helper'
 
-describe Admin::CampaignCampaignsController do
+describe Admin::CampaignsController do
 
   #Delete this example and add some real ones
   it "should use Admin::CampaignCampaignsController" do
-    controller.should be_an_instance_of(Admin::CampaignCampaignsController)
+    controller.should be_an_instance_of(Admin::CampaignsController)
   end
 
 end
