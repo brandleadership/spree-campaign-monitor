19595a9e9b030136a06c30fddf078edb987c6ecc
raskhadafi <roman.simecek@screenconcept.ch>
2010-08-03 11:45:54 +0200

    action for send campaign implemented

--- app/controllers/admin/campaigns_controller.rb
+++ app/controllers/admin/campaigns_controller.rb
@@ -3,5 +3,11 @@ class Admin::CampaignsController < Admin::BaseController
   def index
     @campaigns = Campaign.find(:all)
   end
+
+  def invoke
+    @campaign = Campaign.find(params[:id])
+    @campaign.invoke
+    render :nothing  => true
+  end
   
 end
