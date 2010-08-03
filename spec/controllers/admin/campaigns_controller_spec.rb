56e31061d4db330fb111f880a93826ce9b6f5728
raskhadafi <roman.simecek@screenconcept.ch>
2010-08-03 10:08:10 +0200

    tried to test Admin:CampaignsController without success

--- spec/controllers/admin/campaigns_controller_spec.rb
+++ spec/controllers/admin/campaigns_controller_spec.rb
@@ -1,10 +1,17 @@
 require File.dirname(__FILE__) + '/../../spec_helper'
 
 describe Admin::CampaignsController do
+  integrate_views
+  fixtures :campaigns
 
   #Delete this example and add some real ones
   it "should use Admin::CampaignCampaignsController" do
     controller.should be_an_instance_of(Admin::CampaignsController)
+  end
+
+  it "should show list with index" do
+    get :index
+    response.should render_template("index")
   end
 
 end
